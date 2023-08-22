4f9b3fb5ed788a8c220b743f84cd781040aefb0b
pankecal <pankaj.thapaliya@enginecal.com>
2023-08-22 17:35:53 +0530

    get characteristics based on the battery

--- src/components/Bluetooth.jsx
+++ src/components/Bluetooth.jsx
@@ -12,24 +12,30 @@ let options = {
 
 const BluetoothComponent = () => {
   const [deviceData, setDeviceData] = useState();
-  const [serverData, setServerData] = useState({
-    name: "server data",
-  });
 
-  useEffect(() => {}, [serverData, deviceData]);
+  // UUID to get the battery level
+  let myService = "0000180f-0000-1000-8000-00805f9b34fb";
 
   const myOwnConnection = async () => {
     try {
       const device = await navigator.bluetooth.requestDevice({
         acceptAllDevices: true,
+        optionalServices: [myService],
       });
       const server = await device.gatt.connect();
+      const service = await server.getPrimaryService(myService);
+      const characteristics = await service.getCharacteristics();
 
       const { id, name } = await device;
       const { connected } = await server;
 
       setDeviceData({ id, name, connected });
-      console.log(deviceData);
+
+      console.log("device: ", device);
+      console.log("server: ", server);
+      console.log("service: ", service);
+      console.log("characteristics: ");
+      characteristics.map((c) => console.log(c));
     } catch (err) {
       console.log(err);
     }
